1f6a1c57800ec44009dac2f7beeaa1cee6ef3a13
vishwesh-19 <59824536+vishwesh-19@users.noreply.github.com>
2024-04-22 15:32:02 +0530

    Create wire_decl.v

---
+++ Verilog Language/Basics/wire_decl.v
@@ -0,0 +1,17 @@
+`default_nettype none
+module top_module(
+    input a,
+    input b,
+    input c,
+    input d,
+    output out,
+    output out_n);
+    
+    wire x, y, z;
+    and(x, a, b);
+    and(y, c, d);
+    or(z, x, y);
+
+    assign out = z;
+    assign out_n = ~z;
+endmodule
