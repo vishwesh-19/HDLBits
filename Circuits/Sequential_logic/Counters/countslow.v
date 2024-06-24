3061bd9f58114d9f0193276b78f771475bdb4888
vishwesh-19 <59824536+vishwesh-19@users.noreply.github.com>
2024-06-24 18:47:07 +0530

    Update countslow.v

--- Circuits/Sequential_logic/Counters/countslow.v
+++ Circuits/Sequential_logic/Counters/countslow.v
@@ -1,1 +1,19 @@
+module top_module (
+    input clk,
+    input slowena,
+    input reset,
+    output [3:0] q);
+    
+    always @(posedge clk)   begin
+        if(reset)
+            q <= 4'b0000;
+        
+        else      begin
+            if(slowena)
+                q <= (q==4'd9) ? 4'd0 : q+1;
+            else
+                q <= q;
+        end
+    end
 
+endmodule
