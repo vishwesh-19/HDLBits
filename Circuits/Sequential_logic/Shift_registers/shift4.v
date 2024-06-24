b14c6c79b3a22fe49d2a6f47ba8b41c5f1460f59
vishwesh-19 <59824536+vishwesh-19@users.noreply.github.com>
2024-06-24 18:57:49 +0530

    Create shift4.v

---
+++ Circuits/Sequential_logic/Shift_registers/shift4.v
@@ -0,0 +1,26 @@
+module top_module(
+    input clk,
+    input areset,  // async active-high reset to zero
+    input load,
+    input ena,
+    input [3:0] data,
+    output reg [3:0] q); 
+    
+    always @(posedge clk or posedge areset)     begin
+        if(areset)
+            q <= 4'd0;
+        else     begin
+            if(load)
+                q <= data;
+            
+            else if(~load & ena)  begin
+                q[3] <= 1'b0;
+                q[2] <= q[3];
+                q[1] <= q[2];
+                q[0] <= q[1];
+            end
+            
+        end
+    end
+
+endmodule
