b5ec9932e33cadbcc298eeb4b6365f57034a2cae
vishwesh-19 <59824536+vishwesh-19@users.noreply.github.com>
2024-06-24 15:45:16 +0530

    Create dff8ar.v

---
+++ Circuits/Sequential_logic/Latches_flipflops/dff8ar.v
@@ -0,0 +1,12 @@
+module top_module (
+    input clk,
+    input areset,   // active high asynchronous reset
+    input [7:0] d,
+    output [7:0] q
+);
+     always @ (posedge clk or posedge areset ) begin
+        //this is an active high  asynchronous reset
+        if (areset==1'b1) q<= 1'b0;
+        else q <= d;
+    end
+endmodule
